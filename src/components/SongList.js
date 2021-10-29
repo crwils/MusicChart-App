af3d5ca0fc9ae09166045aaf2c72b39df920a59f
Craig <craigwilsonmusic@gmail.com>
2021-10-29 21:44:40 +0100

    audio working - trying to change background color to white currently

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import SongListItem from './SongListItem';
+import React from "react";
+import SongListItem from "./SongListItem";
 
 const SongList = ({ songs }) => {
-
-    const songItems = songs.map((song, index) => {
-        return <SongListItem song={song} key={index} />
-    })
-
-    return(
-            <div>
-                <ol>
-                    {songItems}
-                </ol>
-            </div>
-    )
+if (songs == null || songs.length === 0) {
+    return <p>Loading...</p>
 }
+
+  return (
+    <div>
+      {songs.map((song, index) => {
+        return (
+          <SongListItem
+            key={song.id.attributes["im:id"]}
+            position={index + 1}
+            title={song["im:name"].label}
+            artist={song["im:artist"].label}
+            image={song["im:image"][1].label}
+            audio={song.link[1].attributes.href}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default SongList;
--- src/components/SongListItem.js
+++ src/components/SongListItem.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const SongListItem = ({song}) => {
+const SongListItem = ({position, title, artist, image, audio}) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioSrc = useRef(null);
+
+    const handlePlayPause = () => {
+        setIsPlaying(!isPlaying);
+    };
+
+    useEffect(() => {
+        isPlaying ? audioSrc.current.play() : audioSrc.current.pause()
+    }, [isPlaying]);
+
     
     return (
-    <li> 
-        <h3>{song['im:artist'].label}</h3>
-        <p>{song['im:name'].label}</p>
-        <img src={song['im:image'][1].label} />
-        <br />
-        <button>
-            <a href={song.link[0].attributes.href}>Preview</a>
-        </button>
-        <hr/>
-    </li>
+    <div className="song-entry-wrapper">
+        <img 
+        style={{height: "50px"}}
+        alt="play button"
+        id={position}
+        src="https://image.freepik.com/free-icon/play-button_318-42541.jpg"
+        onClick={handlePlayPause}
+            />
+        <img alt="song artwork" src={image} />
+        <div className="details">
+            <h3>{position}. {title}</h3>
+            <h4>{artist}</h4>
+        </div>
+        
+        <audio src={audio} ref={audioSrc}/>
+    </div>    
     )
 }
 
